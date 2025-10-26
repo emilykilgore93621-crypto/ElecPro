e936178f82d03c59cc82186c0e9ecf90d6d6c810
Emily Kilgore <emily.kilgore93621@gmail.com>
2025-10-26 03:12:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3090059096-5db8f",
+  "appId": "1:7987230631:web:974b881a8a1ff3a3c2e882",
+  "apiKey": "AIzaSyAP2XfKroCc2mwGSdmtgG8AhUnu2ttIS94",
+  "authDomain": "studio-3090059096-5db8f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "7987230631"
+};
